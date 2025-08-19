444c4373e9b375edc4199ad6732e40f1f632fade
Gregory A Babbitt <gabsbi@rit.edu>
2025-08-18 22:17:35 -0400

    Add files via upload

--- yt-dlp_batch.py
+++ yt-dlp_batch.py
@@ -16,7 +16,9 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 import yt_dlp
-
+import noisereduce as nr
+import soundfile as sf
+import pyloudnorm as pyln
 # IMPORTANT NOTE - run in base conda env, not in atomdance conda env   
 ################################################################################
 
@@ -57,6 +59,24 @@ def main():
     source_folder = "." # Replace with your source folder path
     destination_folder = "YouTube_audio" # Replace with your destination folder path
     move_wav_files(source_folder, destination_folder)
+    # EBU R128 multi-track loudness normalization
+    my_dir = "YouTube_audio"
+    my_dir_new = "YouTube_norm"
+    if not os.path.exists(my_dir_new):
+        os.makedirs(my_dir_new)
+    for filename in os.listdir(my_dir):
+        file_path = os.path.join(my_dir, filename) # Construct full path
+        file_path_new = os.path.join(my_dir_new, filename) # Construct full path
+        print(file_path)
+        print(file_path_new)
+        if os.path.isfile(file_path): 
+            print(f"Reading file: {file_path}")
+            data, rate = sf.read(file_path)
+            meter = pyln.Meter(rate) # Create a BS.1770 loudness meter
+            loudness = meter.integrated_loudness(data) # Measure the integrated loudness of the audio
+            loudness_normalized_audio = pyln.normalize.loudness(data, loudness, -15.0)# Loudness normalize audio to a target LUFS (e.g., -23 LUFS)
+            sf.write(file_path_new, loudness_normalized_audio, rate)# Save the normalized audio
+        
     print("Audio download complete")
 
 ###############################################################
